62fa701294a27ad94b7bd047d5e61a844036345a
Shubham Kapoor <shubham.kapoor01@gmail.com>
2023-03-19 19:41:53 +0530

    added report pddf with graph

--- MLreport/utils.py
+++ MLreport/utils.py
@@ -20,17 +20,21 @@ def calculate_confidence(topic_scores):
     topic_confidence = {}
     for topic, scores in topic_scores.items():
         lower, upper = proportion_confint(scores[0], scores[1], alpha=0.05, method='agresti_coull')
-        topic_confidence[topic] = (lower + upper) / 2
+        topic_confidence[topic] = [lower, upper]
     topic_confidence = sorted(topic_confidence.items(), key=lambda x: x[1])
     return topic_confidence
 
-def get_recommendations(topic_confidence):
+def get_recommendations(topic_confidence, limit=5):
     recommendations = []
+    current_index = 0
     for topic, confidence in topic_confidence:
+        if current_index == limit:
+            break
         recommendations.append({
             "topic": topic,
             "recommendation": call_openai(
-                f"You are helping someone as a data structures instructor, speak directly to them, they has a confidence of {confidence} in the topic {topic}, within 5 sentences explain to them how they should improve and suggest a few practice problems on leetcode, do not mention the confidence level of the student in your response, and just write one paragraph don't use new lines."
+                f"You are helping someone as a data structures instructor, speak directly to them, they have a skill range of {confidence} in the topic {topic}, within 2 short sentences explain to them how they should furthur improve from their current skill level and suggest a few practice problems on leetcode, the difficulty of the suggested problems should align with the skill level of the user, do not mention the skill level of the student in your response, and just write one paragraph don't use new lines."
             )
         })
+        current_index += 1
     return recommendations
--- MLreport/views.py
+++ MLreport/views.py
@@ -1,8 +1,18 @@
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch, cm
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.utils import ImageReader
+import matplotlib.pyplot as plt
+import matplotlib
 from . import utils
-import json
+import textwrap
+import random
+import numpy
+import io
+
+matplotlib.use('agg')
 
 @api_view(['GET'])
 def generate_report(request):
@@ -14,10 +24,51 @@ def generate_report(request):
         'Dynamic Programming': [4, 5],
         'Arrays': [10, 10],
         'Strings': [100, 100],
-		}
+	}
+
     topic_confidence = utils.calculate_confidence(data)
-    recommendations = utils.get_recommendations(topic_confidence)	
-    return Response({
-        'topic_confidence': topic_confidence,
-        'recommendations': recommendations
-    }, status=status.HTTP_200_OK)
+    recommendations = utils.get_recommendations(topic_confidence)
+
+    buffer = io.BytesIO()
+    canvas_object = canvas.Canvas(buffer, pagesize=letter, bottomup=0)
+    text_object = canvas_object.beginText()
+    text_object.setTextOrigin(inch, inch)
+    text_object.setFont('Helvetica', 12)
+    text_object.textLine("Your Report")
+    for i in range(0, 15):
+        text_object.textLine("")
+
+    topics=[]
+    lower=[]
+    upper=[]
+    for topic, scores in topic_confidence:
+        lower.append(scores[0])
+        upper.append(scores[1])
+        topics.append(topic)
+
+    fig = plt.figure(figsize=(16, 9), dpi=200)
+    plt.hist([topics, topics], bins=len(topics), alpha=0.5,
+            label=['lower confidence', 'upper confidence'], weights=[lower, upper], color=['red', 'green'])
+    plt.xticks(range(len(topics)), topics)
+    plt.legend(loc='upper right', fontsize=20)
+
+    imgdata = io.BytesIO()
+    fig.savefig(imgdata, format='png', dpi=200)
+    imgdata.seek(0)
+    Image = ImageReader(imgdata)
+    canvas_object.drawImage(Image, 0, 75, 600, 200)
+
+    for recommendation in recommendations:
+        text_object.textLine(f"{recommendation['topic']}:")
+        lines = textwrap.wrap(recommendation['recommendation'], width=int(1000/text_object._fontsize))
+        for line in lines:
+            padded_line = "{:<{width}}".format(line, width=int(1000/text_object._fontsize))
+            text_object.textLine(padded_line)
+        text_object.textLine("")
+
+    canvas_object.drawText(text_object)
+    canvas_object.showPage()
+    canvas_object.save()
+    buffer.seek(0)
+
+    return FileResponse(buffer, as_attachment=True, filename='your-report.pdf')
